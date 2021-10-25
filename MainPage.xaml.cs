0965def8cf8e6272738018cae9f7161ffb4e42bf
Alexey Strakh <alstrakh@microsoft.com>
2021-10-25 12:44:34 -0700

    Solution to reproduce the issue

---
+++ MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace MauiShadowBreaksLayout
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+	}
+}
